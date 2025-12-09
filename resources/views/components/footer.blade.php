59b2e5a03c5f4ea139cb1f6d44f85c69b570cfb5
Rainiel Mangune <rainielmangune@gmail.com>
2025-12-08 16:16:44 -0800

    new update

--- resources/views/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-</body>
-<div style="width:100%; background-color: red;">
-    copyrigth 2024
-
-</div>
-</html>
--- resources/views/components/mid.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="love">
-    <h2>This is mid component</h2>
-    fhsahfsh
-</div>
